ef717346fd829f35d016440a48305752ea32f12f
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2024-12-23 18:32:32 -0600

    Encoded API URL so that there are no issues with whitespace.
    Encoded API URL so that there are no issues with whitespace or nonstandard characters. Also made a drop down list of each country using the data in my MySQL server.

--- backend/src/main/java/com/gfsiproject/gfsi/controller/CountryController.java
+++ backend/src/main/java/com/gfsiproject/gfsi/controller/CountryController.java
@@ -1,5 +1,7 @@
 package com.gfsiproject.gfsi.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,21 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gfsiproject.gfsi.entity.Country;
 import com.gfsiproject.gfsi.service.CountryService;
 
-
 @RestController
 public class CountryController {
 
     @Autowired
     private CountryService countryservice;
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/countries")
     public List<Country> getCountries(){
         return this.countryservice.getCountryData();
     }
 
-    @GetMapping("/countries/{country_name}")
     @CrossOrigin(origins = "http://localhost:5173")
+    @GetMapping("/countries/{country_name}")
+    @SuppressWarnings("CallToPrintStackTrace")
     public Country getCountriesByName(@PathVariable String country_name){
+        try { 
+            country_name = URLDecoder.decode(country_name, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         return this.countryservice.getCountryByName(country_name);
     }
 }
--- frontend/my-react-app/src/App.tsx
+++ frontend/my-react-app/src/App.tsx
@@ -1,25 +1,48 @@
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const getCountry = () => {
-    axios.get('http://localhost:8080/countries/Algeria')
-    .then(res => {
-      console.log(res)
-    })
-    .catch(err => {
-      console.log(err)
-    }) 
+  interface CountryData {
+    country: string;
+    overall: number;
+    affordability: number;
+    availability: number;
+    quality: number;
+    sustainability: number;
+    overallrank: number;
+    affordabilityrank: number;
+    availabilityrank: number;
+    qualityrank: number;
+    sustainabilityrank: number;
   }
+
+  const [countries, setCountries] = useState<CountryData[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/countries") 
+      .then((response) => {
+        setCountries(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching countries:", error);
+      });
+  }, []);
 
   return (
     <>
       <div>
         <p className="text-blue-100 bg-blue-500 text-4xl">Hello</p>
+        <select className="border rounded p-2">
+          {countries.map((country, index) => (
+            <option key={index} value={country.country}>
+              {country.country}
+            </option>
+          ))}
+        </select>
       </div>
-      <button onClick={getCountry}>Get Country</button>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
